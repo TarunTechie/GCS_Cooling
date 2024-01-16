ed897cf34276e2e95059c6219baf36b01e99378e
tarun-techie <tarsup25@gmail.com>
2024-01-17 00:34:40 +0530

    changes madee to gallery and products pages for responsiveness

--- src/components/ptop.jsx
+++ src/components/ptop.jsx
@@ -4,7 +4,7 @@ export default function PTop()
     return(<div className="fixed">
         <div className="flex bg-white w-screen">
         <a href="/"><img src="\assets\logo.png" alt="GCS-COOLING" className="max-w-20 max-h-20" /></a>
-        <a href="/"><div className="md:text-2xl text-center font-bold text-white bg-blue-500 rounded-full p-4 mb-2 mt-2 ml-4"><h1>GANGA COOLING SOLUTIONS</h1></div></a>
+        <a href="/"><div className="hidden md:text-2xl text-center font-bold text-white bg-blue-500 rounded-full p-4 mb-2 mt-2 ml-4"><h1>GANGA COOLING SOLUTIONS</h1></div></a>
         <div className="md:text-2xl text-center font-bold text-blue-500 rounded-full p-4 mb-2 mt-2 ml-4">PRODUCTS</div>
         </div>
         <div className="flex flex-row bg-blue-500 place-content-around gap-20 fixed w-screen header">
--- src/gal.jsx
+++ src/gal.jsx
@@ -4,7 +4,7 @@ export default function Gall()
     let slide=["/assets/gallary/gal (1).JPG","/assets/gallary/gal (2).JPG","/assets/gallary/gal (3).JPG",
     "/assets/gallary/gal (4).JPG","/assets/gallary/gal (5).JPG","/assets/gallary/gal (6).JPG","/assets/gallary/gal (7).JPG"
     ,"/assets/gallary/gal (8).JPG","/assets/gallary/gal (9).JPG","/assets/gallary/gal (10).JPG","/assets/gallary/gal (11).JPG","/assets/gallary/gal (12).JPG"
-    ,"/assets/gallary/gal (13).JPG","/assets/gallary/gal (14).JPG",,"/assets/gallary/gal (15).JPG","/assets/gallary/gal (16).JPG"]
+    ,"/assets/gallary/gal (13).JPG","/assets/gallary/gal (14).JPG",,"/assets/gallary/gal (15).JPG"]
     return (<div>
         <Galtop/>
         <div  className="gala">
